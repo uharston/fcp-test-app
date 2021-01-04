f5b439a1ddd45055a3ad517f8042ed29718db1cd
uharston <uharston@gmail.com>
2021-01-04 13:30:24 -0600

    Delete Error Handling

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -3,15 +3,6 @@ class SubscribersController < ApplicationController
         @subscriber =  Subscriber.create(email: subscriber_params[:email])
         if @subscriber.valid? 
             SubscriberMailer.with(subscriber: @subscriber).welcome_email.deliver_now
-        # send success message 
-        # send email
-        # render template: "welcome/home"
-
-        else  
-            # flash[:errors] = @subscriber.errors
-
-        # send error message
-        # render template: "welcome/home"
         end 
     end 
 
