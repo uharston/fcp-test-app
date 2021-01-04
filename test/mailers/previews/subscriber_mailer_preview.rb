c97935897c967a2012d1da1060567ba04be97e2d
uharston <uharston@gmail.com>
2021-01-04 12:04:30 -0600

    Create Test View For Welcome Email

--- test/mailers/previews/subscriber_mailer_preview.rb
+++ test/mailers/previews/subscriber_mailer_preview.rb
@@ -1,4 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/subscriber_mailer
 class SubscriberMailerPreview < ActionMailer::Preview
-
+    def welcome_email 
+        SubscriberMailer.with(subscriber: Subscriber.first).welcome_email
+    end
 end
