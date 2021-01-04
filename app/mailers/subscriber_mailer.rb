1182b13200a753b20118168fcc38cad54e23acab
uharston <uharston@gmail.com>
2021-01-04 12:50:21 -0600

    Add SubscriberMailer With Welcome Email Action

--- app/mailers/subscriber_mailer.rb
+++ app/mailers/subscriber_mailer.rb
@@ -1,2 +1,8 @@
 class SubscriberMailer < ApplicationMailer
+    default from: 'uharston@gmail.com' 
+
+    def welcome_email 
+        @subscriber = params[:subscriber]
+        mail(to: @subscriber.email, subject: "Welcome To Benz Parts Subscriber List!")
+    end
 end
