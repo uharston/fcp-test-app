c5d4ab10d0fa1b17ed2f157b4198de075d6135c2
uharston <uharston@gmail.com>
2021-01-04 11:19:45 -0600

    Create Action And Protected Params Method

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -1,4 +1,17 @@
 class SubscribersController < ApplicationController
+    def create 
+        byebug
+        @subscriber =  Subscriber.create(email: subscriber_params[:email])
+        if @subscriber.valid? 
+        # send success message 
+        # send email
+        else  
+        # send error message
+        end 
+    end 
 
+    def subscriber_params 
+        params.require(:subscriber).permit(:email)
+    end 
  
 end
