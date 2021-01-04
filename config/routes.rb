c0bbd5c6598f7be45450b89b1d1be079ab335e6d
uharston <uharston@gmail.com>
2021-01-04 16:45:44 -0600

    Chat Index Route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "welcome#home"
   post '/', to: 'subscribers#create', as: "subscribers"
+  resources :chats, only: %i[index]
 end
