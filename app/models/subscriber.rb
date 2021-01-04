5af65a44ab9ebe4b9753eb9c693be715b86ea038
uharston <uharston@gmail.com>
2021-01-04 11:17:29 -0600

    Add Email Uniqueness Validation

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -1,4 +1,5 @@
 class Subscriber < ApplicationRecord
     validates :email, presence: true
+    validates :email, uniqueness: { case_sensitive: false }
     validates :email, length: { maximum: 40 }
 end
